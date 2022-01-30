5fff7ee148b9a5f25917d9e37693f7cb31fb0057
okskurnosova <oks.belysheva@yandex.ru>
2022-01-30 22:33:24 +0400

    Add carousel Swiper

--- js/common.js
+++ js/common.js
@@ -1,5 +1,10 @@
 document.addEventListener('DOMContentLoaded', function() {
 
+  const showSwiper = new Swiper('.showcase-carousel', {
+    loop: true,
+    slidesPerView: 3,
+    speed: 1800 
+  })
 
   document.querySelector('video').playbackRate = 2;
 })
