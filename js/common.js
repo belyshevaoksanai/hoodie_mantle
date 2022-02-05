40d8fad8ea240a0403ec6b2ce3655b2e2dc7b2ba
okskurnosova <oks.belysheva@yandex.ru>
2022-02-05 09:00:20 +0400

    Add images

--- js/common.js
+++ js/common.js
@@ -3,7 +3,8 @@ document.addEventListener('DOMContentLoaded', function() {
   const showSwiper = new Swiper('.showcase-carousel', {
     loop: true,
     slidesPerView: 3,
-    speed: 1800 
+    speed: 1800,
+    centeredSlides: true 
   })
 
   document.querySelector('video').playbackRate = 2;
